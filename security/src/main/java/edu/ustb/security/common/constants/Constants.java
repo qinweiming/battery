473bc097c9d80cf70389d0e0639ae0a2c767fc0d
yichaosun <sunyichao_believe@yahoo.com>
2016-12-20 20:08:17 +0800

    孙益超：CPK算法依赖的常量、工具类

---
+++ security/src/main/java/edu/ustb/security/common/constants/Constants.java
@@ -0,0 +1,9 @@
+package edu.ustb.security.common.constants;
+
+/**
+ * Created by sunyichao on 2016/12/20.
+ */
+public class Constants {
+    public static final String CHARSET = "UTF-8";
+    public static final String MD = "SHA-256";
+}
