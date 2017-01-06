ffeff44c6150f03ae194360889ac30512bbf542a
xudongmei <xdm920508>
2017-01-06 12:08:55 +0800

    添加面向政府人员的API实现

--- service/app/controllers/v1/Certs.java
+++ service/app/controllers/v1/Certs.java
@@ -2,13 +2,16 @@ package controllers.v1;
 
 import controllers.api.API;
 import models.Cert;
+import models.Search;
 import org.bson.types.ObjectId;
 import play.Logger;
 import play.Play;
 import play.data.validation.Required;
 import play.libs.Files;
+import utils.StringUtils;
 
 import java.io.File;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -16,7 +19,6 @@ import java.util.stream.StreamSupport;
 import static play.modules.jongo.BaseModel.getCollection;
 
 /**
- * 证书申请/审批/查看
  * Created by xudongmei on 2016/12/13.
  */
 @SuppressWarnings("ALL")
@@ -24,60 +26,77 @@ public class Certs extends API {
 
     public static String storePath = Play.configuration.getProperty("attachments.path");
     /**
-     * 申请证书
+     * 1.政府内部用户-离线信息录入（申请证书）
      */
-    public static void save() {
+    public static void apply() {
         Cert cert = readBody(Cert.class);
+        cert.createTime = new Date();//添加创建时间
+        cert.modifyTime = new Date();//添加修改时间
         cert.save();
     }
 
     /**
-     * 查看所有已申请的证书信息
-     * @param filters
-     * @param limit
-     * @param offset
+     * 2.政府内部用户-申请状态查询（status=0待审批；status=1审批通过；status=2审批不通过）
+     * 通过 status 的取值进入到待审批列表、已审批列表
      */
-    public static void list(@Required String filters,Integer limit,Integer offset) {
-        List<Cert> certs =  StreamSupport.stream(getCollection(Cert.class).find().limit(limit).as(Cert.class).spliterator(),false).collect(Collectors.toList());
-        certs.forEach(cert -> Logger.info("List：" + cert.companyName));
+    public static void list(@Required Integer status) {
+        Integer limit = Integer.valueOf(request.params.get("limit"));
+        Integer offset = Integer.valueOf(request.params.get("offset"));
+        Search search = readFilters(Search.class);//获取列表页的查询条件
+        /**
+         * sql查询语句
+         * companyName、startDate、endDate为查询条件
+         */
+        String sql = "{status: " + status;
+        if(StringUtils.isNotNullOrEmpty(search.companyName)) {
+            sql += ",companyName: " + search.companyName;
+        }else if(search.startDate != null && search.endDate != null) {
+            sql += ",createTime: {\"$gte\": " + search.startDate + ", \"$lte\": " + search.endDate + "}";
+        }else if(search.startDate != null) {
+            sql += ",createTime: {\"$gte\": " + search.startDate + "}";
+        }else if(search.endDate != null) {
+            sql += ",createTime: {\"$lte\": " + search.endDate + "}";
+        }
+        sql += "}";
+        Logger.info("sql: " + sql);
+        Logger.info("search.startDate: " + search.startDate);
+        Logger.info("search.endDate: " + search.endDate);
+        List<Cert> certs = StreamSupport.stream(getCollection(Cert.class).find(sql).limit(limit).skip(offset).as(Cert.class).spliterator(),false).collect(Collectors.toList());
         renderJSON(certs);
     }
 
-    public static void approve(@Required String ids, @Required Integer status){
+    /**
+     * 2.1 政府内部用户-证书审批
+     * @param ids
+     */
+    public static void approve(@Required String ids) {
+        Integer status = Integer.valueOf(request.params.get("status"));
         String[] idArr = ids.split(",");
+        Date modifyTime = new Date();
+        Logger.info("modifyTime: " + modifyTime);
+        /**
+         * 没考虑 id 不存在的情况
+         */
         for(String id : idArr) {
-            Cert cert = getCollection(Cert.class).findOne(new ObjectId(id)).as(Cert.class);
-            if (cert == null) {
-                notFound(id);
-            } else {
-                getCollection(Cert.class).update("{status : " + cert.status + "}").with("{$set:{status:" + status + "}}");
-            }
+            getCollection(Cert.class).update(new ObjectId(id)).with("{$set:{status:#,modifyTime:#}}",status,modifyTime).isUpdateOfExisting();
         }
     }
-    
+
     /**
-     * 我的申请
-     * @param id
+     * 3.我的申请
+     * @param companyId 企业的Id
      */
-    public static void get(@Required String id){
-        Cert cert =  getCollection(Cert.class).findOne(new ObjectId(id)).as(Cert.class);
+    public static void get(@Required String companyId){
+        Cert cert =  getCollection(Cert.class).findOne("{companyId:#}",companyId).as(Cert.class);
         if (cert == null) {
-            notFound(id);
+            notFound(companyId);
         }else {
             renderJSON(cert);
         }
     }
 
     /**
-     * 申请查询（status=0待审批；status=1审批通过；status=2审批不通过）
-     * @param status
-     */
-    public static void search(@Required Integer status){
-        renderJSON(getCollection(Cert.class).find("{status:#}",status));
-    }
-
-    /**
-     * 证书导入
+     * 4.证书导入
      * @param attachment
      * @param remark
      */
@@ -86,7 +105,7 @@ public class Certs extends API {
     }
 
     /**
-     * 附件上传
+     * 5.附件上传
      * @param attachment
      */
     public static void attachment(@Required File attachment) {
@@ -112,22 +131,5 @@ public class Certs extends API {
 
     }
 
-    /**
-     * 获取流量分析图
-     * @param beginModuleId
-     * @param endModuleId
-     */
-    public static void flowAnalysis(@Required String beginModuleId,@Required String endModuleId) {
-
-    }
-
-    /**
-     * 获取密度分析图
-     * @param beginModuleId
-     * @param endModuleId
-     */
-    public static void densityAnalysis(@Required String beginModuleId,@Required String endModuleId) {
-
-    }
 
 }
---
+++ service/app/controllers/v1/Trades.java
@@ -0,0 +1,137 @@
+package controllers.v1;
+
+import controllers.api.API;
+import models.*;
+import models.Package;
+import org.bson.types.ObjectId;
+import play.Logger;
+import play.data.validation.Required;
+import utils.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static play.modules.jongo.BaseModel.getCollection;
+
+/**
+ * Created by xudongmei on 2016/12/13.
+ */
+public class Trades extends API {
+    /**
+     * 1.新增交易信息
+     */
+    public static void save() {
+        Trade trade = readBody(Trade.class);
+        trade.save();
+    }
+
+    /**
+     * 2.获取交易信息
+     */
+    public static void list() {
+        Integer limit = Integer.valueOf(request.params.get("limit"));
+        Integer offset = Integer.valueOf(request.params.get("offset"));
+        Search search = readFilters(Search.class);//获取列表页的查询条件
+        /**
+         * sql查询语句
+         * fromId、toId、startDate、endDate、startProductId、endProductId为查询条件
+         */
+        String sql = "";
+        if(StringUtils.isNotNullOrEmpty(search.fromId)) {
+            sql += "{fromId: " + search.fromId;
+        }if(StringUtils.isNotNullOrEmpty(search.toId)) {
+            sql += ",toId: " + search.toId;
+        }else if(search.startDate != null && search.endDate != null) {
+            sql += ",createTime: {\"$gte\": " + search.startDate + ", \"$lte\": " + search.endDate + "}";
+        }else if(search.startDate != null) {
+            sql += ",createTime: {\"$gte\": " + search.startDate + "}";
+        }else if(search.endDate != null) {
+            sql += ",createTime: {\"$lte\": " + search.endDate + "}";
+        }else if(search.startProductId != null && search.endProductId != null) {
+            sql += ",productIds: {\"$gte\": " + search.startProductId + ", \"$lte\": " + search.endProductId + "}";
+        }else if(search.startProductId != null) {
+            sql += ",productIds: {\"$gte\": " + search.startProductId + "}";
+        }else if(search.endProductId != null) {
+            sql += ",productIds: {\"$lte\": " + search.endProductId + "}";
+        }
+        sql += "}";
+        Logger.info("sql: " + sql);
+        List<Trades> trades = StreamSupport.stream(getCollection(Trades.class).find(sql).limit(limit).skip(offset).as(Trades.class).spliterator(),false).collect(Collectors.toList());
+        renderJSON(trades);
+    }
+
+    /**
+     * 3.获取单个交易信息
+     * @param id
+     */
+    public static void get(@Required String id) {
+        Trade trade =  getCollection(Trade.class).findOne(new ObjectId(id)).as(Trade.class);
+        if (trade == null) {
+            notFound(id);
+        }else {
+            renderJSON(trade);
+        }
+    }
+
+    /**
+     * 4.获取流量分析图
+     * @param beginModuleId
+     * @param endModuleId
+     */
+    public static void flowAnalysis(@Required String beginModuleId,@Required String endModuleId) {
+        String sql = "{moduleId: {\"$gte\": " + beginModuleId + ", \"$lte\": " + endModuleId + "}}";
+        List<Module> modules = StreamSupport.stream(getCollection(Module.class).find(sql).as(Module.class).spliterator(),false).collect(Collectors.toList());
+        renderJSON(modules);
+    }
+
+    /**
+     * 5.获取密度分析图
+     * @param beginModuleId
+     * @param endModuleId
+     */
+    public static void densityAnalysis(@Required String beginModuleId,@Required String endModuleId) {
+        flowAnalysis(beginModuleId,endModuleId);
+    }
+
+    /**
+     * 6.上传电池包（与模组对应关系）数据
+     */
+    public static void packageAndModule() {
+        Package packages = readBody(Package.class);
+        packages.save();
+    }
+    /**
+     * 7.汽车制造商上传汽车与电池包的对应关系
+     */
+    public static void carAndPackage() {
+        Car cars = readBody(Car.class);
+        cars.save();
+    }
+
+    /**
+     * 8.获取电池包信息
+     * @param id packageId
+     */
+    public static void getPackage(@Required String id){
+        Package Package =  getCollection(Package.class).findOne("{packageId:#}",id).as(Package.class);
+        if (Package == null) {
+            notFound(id);
+        }else {
+            renderJSON(Package);
+        }
+    }
+
+    /**
+     * 9.获取汽车信息
+     * @param id carId
+     */
+    public static void getCar(@Required String id){
+        Car car =  getCollection(Car.class).findOne("{carId:#}",id).as(Car.class);
+        if (car == null) {
+            notFound(id);
+        }else {
+            renderJSON(car);
+        }
+    }
+}
