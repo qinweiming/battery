060b06eb40c82dad833fa3964043cefcc1660a98
xudongmei <xdm920508>
2017-01-15 18:18:08 +0800

    xudongmei：CPK证书生成；解压缩文件

--- service/app/controllers/v1/Certs.java
+++ service/app/controllers/v1/Certs.java
@@ -5,13 +5,21 @@ import controllers.api.API;
 import models.Cert;
 import models.Module;
 import org.apache.commons.lang.StringUtils;
+
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import org.apache.ivy.osgi.util.ZipUtil;
 import org.bson.types.ObjectId;
 import org.jongo.MongoCursor;
+import play.Logger;
 import play.Play;
 import play.data.binding.As;
 import play.data.validation.Range;
 import play.data.validation.Required;
 import play.libs.Files;
+import play.libs.IO;
 import play.vfs.VirtualFile;
 import utils.SafeGuard;
 
@@ -23,7 +31,9 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+import java.util.zip.ZipOutputStream;
 
+import static java.lang.System.in;
 import static play.modules.jongo.BaseModel.getCollection;
 
 /**
@@ -33,7 +43,10 @@ import static play.modules.jongo.BaseModel.getCollection;
 public class Certs extends API {
 
     public static String certFilesPath = Play.configuration.getProperty("attachments.path") + File.separator + "certs";
-    public static String QRPath = Play.configuration.getProperty("attachments.path") + File.separator + "QRs";//二维码地址
+    public static String uploadsPath = certFilesPath + File.separator + "uploads";//上传的法人证书保存地址
+    public static String producesPath = certFilesPath + File.separator + "produces";//保存审批后生成法人CPK证书
+
+    public static String QRPath = certFilesPath + File.separator + "QRs";//二维码图片存放地址
     public static String pattern = "yyyy-MM-dd";
 
     /**
@@ -74,7 +87,7 @@ public class Certs extends API {
 
     /**
      * 2.1 政府内部用户-证书审批
-     * @param ids
+     * @param ids 证书Ids
      */
     public static void approve(@Required String ids) throws IOException {
 
@@ -88,21 +101,53 @@ public class Certs extends API {
             /**
              * @Description
              *  1.调用接口生成 tradeSk,productSK,Matrix
-             *  2.将3个各生成一个文件、
-             *  3.将文件压缩成一个zip包
+             *  2.将3个字符串各生成一个文件
+             *  3.将2中的文件压缩成一个zip包
              */
          /*   String tradeSk = generateSkById(社会信用码).toString;
             String productSK = generateSkById(厂商代码).toString;
-            SeedMatrix matrix = CpkMatrixsFactory.generateCpkMatrix();
-            String[] sk = {tradeSk,productSK};
-
-            String filePath = certFilesPath + File.separator + id;
-            String fileName = id + "_tradeSk";
-            writeFile(filePath, fileName,sk);*/
-            //compress();
+            String matrix = CpkMatrixsFactory.generateCpkMatrix().toJson();
+            */
             /**
-             * 实现文件压缩，并将源文件删除
+             * 将 3个字符串分别写入到 3个文件中
              */
+            String tradeSk = "wwww";
+            String productSK = "aaaaa";
+            String matrix = "ssss";
+            String[] strs = {tradeSk,productSK,matrix};
+            for(String str: strs) {
+                IO.write(str.getBytes(),new File(producesPath + File.separator + UUID.randomUUID() + ".txt"));
+            }
+            /**
+             * 将这 3个文件打包为一个zip,并删除初始的 3个文件
+             */
+            File file = new File(producesPath);
+            File[] fileList = file.listFiles();//读取produces目录下生成的3个CPK证书
+            File zipFile = new File(producesPath + File.separator + UUID.randomUUID() + ".zip");
+            for(int i=0;i<fileList.length;i++) {
+                Files.copy(fileList[i],zipFile);
+                //Files.delete(fileList[i]);
+            }
+
+/*
+            FileOutputStream outputStream = new FileOutputStream(zipFile);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(outputStream));
+            FileInputStream fis = new FileInputStream(file);
+
+            for(int i=0;i<fileList.length;i++) {
+                //Files.copy(fileList[i],zipFile);
+                //Files.delete(fileList[i]);
+                out.putNextEntry(new ZipEntry(""));
+                //进行写操作
+                int j = 0;
+                byte[] buffer = new byte[1024];
+                while((j = fis.read(buffer)) > 0) {
+                    out.write(buffer,0,j);
+                }
+                //关闭输入流
+                fis.close();
+                out.close();
+            }*/
         }
     }
 
@@ -139,21 +184,49 @@ public class Certs extends API {
 
     /**
      * 5.附件上传
-     * @param attachment
+     * @param attachment 上传的附件
      */
-    public static void attachment(File attachment) throws FileNotFoundException {
+    public static void attachment(File attachment) throws IOException {
         //todo: 通用的附件上传
-//        new FileInputStream(attachment),
-//                MimeTypes.getContentType(attachment.getName());
+        File storeFile = new File(uploadsPath + File.separator + UUID.randomUUID());//上传文件的保存地址
+        Logger.info("upload file name：" + attachment.getName());
+        Boolean b = attachment.getName().endsWith(".zip");
+        if(b) {//上传的是zip,需要解压
+            File pathFile = new File(uploadsPath);
+            if(!pathFile.exists()) {
+                pathFile.mkdirs();
+            }
+            ZipFile zip = new ZipFile(attachment);
+            for(Enumeration entries = zip.entries(); entries.hasMoreElements();) {
+                ZipEntry entry = (ZipEntry)entries.nextElement();
+                String zipEntryName = entry.getName();
+                String outPath = (uploadsPath + File.separator + zipEntryName).replaceAll("\\*","/");
+                File file = new File(outPath.substring(0,outPath.lastIndexOf("/")));
 
-        File storeFile = new File(certFilesPath + File.separator + UUID.randomUUID());
+                if(!file.exists()) {
+                    file.mkdirs();
+                }else if(new File(outPath).isDirectory()) {
+                    continue;
+                }
+                Logger.debug("file's Path: " + outPath);
+                OutputStream out = new FileOutputStream(outPath);
+                byte[] buf = new byte[1024];
+                int len;
+                while((len = in.read(buf)) > 0) {
+                    out.write(buf,0,len);
+                }
+                in.close();
+                out.close();
+            }
+            Files.delete(attachment);
+        }
         Files.copy(attachment, storeFile);
         renderJSON("{\"success\":\"ok\"}");
     }
 
     /**
      *  证书(压缩包文件)下载
-     * @param id
+     * @param id 证书id
      */
     public static void download(@Required String id) {
         Cert cert = getCollection(Cert.class).findOne(new ObjectId(id)).as(Cert.class);
@@ -163,13 +236,13 @@ public class Certs extends API {
 
     /**
      * 单个二维码生成
-     * @param moduleId
+     * @param moduleId 流水号
      */
     public static void singleCode(@Required String moduleId) throws IOException {
         /**
          * 调用sign(BigInteger sk,String moduleId)接口生成签名
          */
-     /*   String str = "22223444444444444444";
+     /*   String str = "2222344453532";
         BigInteger sk = new BigInteger(str);//BigInteger类型的私钥
         Pair pair = sign(sk,moduleId);//根据私钥、电池编号(流水号)生成签名信息
 
@@ -189,8 +262,8 @@ public class Certs extends API {
 
     /**
      * 批量二维码生成
-     * @param beginModuleId
-     * @param endModuleId
+     * @param beginModuleId 初始流水号
+     * @param endModuleId 截止流水号
      */
     public static void batchCode(@Required String beginModuleId,@Required String endModuleId) throws IOException {
         List<Module> modules = StreamSupport.stream(getCollection(Module.class).find("moduleId:{$gte:#},moduleId:{$lte:#}}",beginModuleId,endModuleId).as(Module.class).spliterator(),false).collect(Collectors.toList());
